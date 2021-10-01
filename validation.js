4b2b4c041a77089d3264b82a3513a14dcbbdd936
Tang <troy1636@gmail.com>
2021-10-01 09:48:49 -0400

    really didnt do anything

---
+++ validation.js
@@ -0,0 +1,15 @@
+function validate() {}
+
+function vUsername() {
+	var oForm = document.getElementById("username");
+	var value = oForm.value;
+	if (value.match()) {
+	}
+}
+function vEmail() {}
+function vPhoneNum() {}
+function vPassword() {}
+function confirmPassword(vpassword) {}
+function vGender() {}
+function vAge() {}
+function vMusic() {}
